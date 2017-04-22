8815a71c57b0020d610e4eafd1d7ef57ffd6dc49
SrushtiGangireddy <srushti.gangireddy@gmail.com>
2017-04-22 14:51:28 -0700

    ssl database change

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 import urlparse
 
-DATABASE_URL='postgres://ehddgphisfwedk:a79251b8a465c1f65ab7d62f13d63c68ec2e439a01ff1fb60c634ccb62f72c4f@ec2-23-23-237-68.compute-1.amazonaws.com:5432/d3h38vj40ov5bk?sslmode=required'
+DATABASE_URL='postgres://ehddgphisfwedk:a79251b8a465c1f65ab7d62f13d63c68ec2e439a01ff1fb60c634ccb62f72c4f@ec2-23-23-237-68.compute-1.amazonaws.com:5432/d3h38vj40ov5bk?sslmode=require'
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
