b3e8143a6c431b396ac23b45d6a2ccde1cf005ab
SrushtiGangireddy <srushti.gangireddy@gmail.com>
2017-04-24 16:47:59 -0700

    Changes in projects explore page

--- Flask_Demo.py
+++ Flask_Demo.py
@@ -255,7 +255,6 @@ def reward():
 @app.route('/donate',methods=['POST','GET'])
 def donate():
     if request.method == 'GET':
-
         return render_template('donateAmount.html')
     elif request.method == 'POST':
         id = request.form.get('projectID')
