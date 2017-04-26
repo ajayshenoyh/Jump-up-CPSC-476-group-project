36d94165646ec5b33426db21258da52063b4a5a2
ajays <ajayshenoy@csu.fullerton.edu>
2017-04-25 20:32:44 -0700

    Password encryption

--- Flask_Demo.py
+++ Flask_Demo.py
@@ -6,8 +6,7 @@ from wtforms import Form, TextField, validators, PasswordField, BooleanField
 # from flask_login import login_user, logout_user
 #login_manager = LoginManager()
 #login_manager.init_app(app)
-from cryptoshop import encryptstring
-from cryptoshop import decryptstring
+import pyaes
 #from psycopg2.extensions import adapt as thwart
 from datetime import datetime
 from flask import request
