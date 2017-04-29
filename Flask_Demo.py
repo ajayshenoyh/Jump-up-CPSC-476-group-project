3e7ba1100b2202bf3fd4778510773335a60e6ad0
SrushtiGangireddy <srushti.gangireddy@gmail.com>
2017-04-28 22:58:41 -0700

    Adding dashboard

--- Flask_Demo.py
+++ Flask_Demo.py
@@ -94,7 +94,7 @@ def login():
         else:
             pwd = user_details[0][1]
             print(pwd)
-            print(pwd.decode(encoding='UTF-8'))
+            print(pwd.encode(encoding='UTF-8'))
             decrpted_password = f.decrypt(pwd.encode(encoding='UTF-8'))
             if password == decrpted_password:
                 Session['UserName'] = username
